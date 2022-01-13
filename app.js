a47d5a4a824cf303f4191ddfac7957f429a8ed53
Aryan Dixit <aryan.dixit.026@gmail.com>
2022-01-13 14:11:20 +0530

    commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express();
+const path = require('path');
+const fs = require('fs');
+const port = 80;
+
+// Express
+
+app.use('/static', express.static("static"));
+app.use(express.urlencoded());
+
+// Pug Specific
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, "views"));
+
+
+app.get("/", (req,res)=>{
+
+    res.render("base.pug");
+
+})
+
+
+
+
+
+
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`The port is running at ${port}`);
+})
